f9844fcb4cc5b6bbcecd45b347680b4ae6364240
Jimmy Lai <56391923+jkclai@users.noreply.github.com>
2020-04-03 17:32:27 -0700

    Set deb install to default in chrome.sh

--- src/applications/chrome.sh
+++ src/applications/chrome.sh
@@ -6,3 +6,5 @@ install_chrome_deb () {
     wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
     sudo dpkg -i google-chrome-stable_current_amd64.deb
 }
+
+install_chrome_deb
