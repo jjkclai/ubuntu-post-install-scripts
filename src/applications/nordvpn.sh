7d05b18e092107b75ff3ca3e93a6dbfd4e95ac1b
Jimmy Lai <56391923+jkclai@users.noreply.github.com>
2020-04-03 17:17:47 -0700

    Implement deb install function in nordvpn.sh

--- src/applications/nordvpn.sh
+++ src/applications/nordvpn.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+
+install_nordvpn_deb () {
+    cd /tmp/
+
+    wget https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+    sudo dpkg -i nordvpn-release_1.0.0_all.deb
+
+    sudo apt update
+    sudo apt -y install nordvpn
+}
