867480366ef830b0efc4f55cbbaa755021880112
Jimmy Lai <56391923+jkclai@users.noreply.github.com>
2020-04-05 23:26:38 -0700

    Implement archive install function in fontawesome.sh

--- src/applications/fontawesome.sh
+++ src/applications/fontawesome.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+install_fontawesome_archive () {
+    cd /tmp/
+
+    file_link="https://github.com$( wget "https://github.com/ngdanghau/fontawesome-pro/releases/latest" -q -O - | sed -ne 's@.*\(\/ngdanghau\/fontawesome-pro\/.*zip\).*@\1@p' | head -1 )"
+    file_name=$( basename $file_link )
+
+    wget $file_link
+
+    unzip $file_name
+    unzip fontawesome*desktop.zip
+
+    cp -r /tmp/fontawesome*desktop/otfs/*.otf ~/.local/share/fonts/
+
+    fc-cache
+}
