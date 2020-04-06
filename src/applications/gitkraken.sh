713e1114210dfb86116ca0bd6023fc47febf10b5
Jimmy Lai <56391923+jkclai@users.noreply.github.com>
2020-04-05 23:37:24 -0700

    Implement deb install function in gitkraken.sh

--- src/applications/gitkraken.sh
+++ src/applications/gitkraken.sh
@@ -3,3 +3,10 @@
 install_gitkraken_snap () {
     sudo snap install gitkraken
 }
+
+install_gitkraken_deb () {
+    cd /tmp/
+    
+    wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+    sudo dpkg -i gitkraken-amd64.deb
+}
