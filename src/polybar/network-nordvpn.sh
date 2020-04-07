630ebd011d8152e379368d8e8b59b551eefe8a4d
Jimmy Lai <56391923+jkclai@users.noreply.github.com>
2020-04-07 11:05:11 -0700

    Implement network-nordvpn.sh

--- src/polybar/network-nordvpn.sh
+++ src/polybar/network-nordvpn.sh
@@ -1,1 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+
+STATUS=$( nordvpn status | grep Status | tr -d ' ' | cut -d ':' -f2 )
+
+if [ "$STATUS" = "Connected" ]; then
+    echo "%{A1:nordvpn set killswitch off; nordvpn d:}$(nordvpn status | grep City | cut -d ':' -f2) ($(curl -sf ifconfig.co))%{A}"
+else
+    echo "%{A1:nordvpn set killswitch on; nordvpn c:}Disconnected ($(curl -sf ifconfig.co))%{A}"
+fi
